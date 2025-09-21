c8d0b9395372e10f430eb3596e921a8c194dca3e
VasudevaSurf <vasudev@surf.mt>
2025-09-22 00:19:12 +0530

    done

--- middleware/cors.js
+++ middleware/cors.js
@@ -7,6 +7,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
       "http://localhost:3001",
       "http://127.0.0.1:3000",
       "http://127.0.0.1:3001",
+      "https://block-pal.vercel.app",
     ];
 
 const corsOptions = {
